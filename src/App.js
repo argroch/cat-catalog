fef321d514f809cac35ffc362b15dbbdc55a18ad
Aaron Groch <ar.groch@gmail.com>
2022-02-05 17:44:18 -0800

    a catalog of random cats. aww.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
+import { loremIpsum } from "lorem-ipsum";
 
 import './App.css';
 
@@ -9,15 +10,23 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [],
+      cats: [],
       searchField: ''
     }
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+    const catNames = require('cat-names');
+    let kitties = [];
+    for (var i = 1; i < 13; i++){
+      let kitty = {id: i, name: catNames.random(), description: loremIpsum()};
+      kitties.push(kitty);
+    }
+    // Old way of populating cats: User API...
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(users => this.setState({ cats: users }))
+    this.setState({ cats: kitties });
   }
 
   handleChange = e => {
@@ -25,18 +34,18 @@ class App extends Component {
   }
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
+    const { cats, searchField } = this.state;
+    const filteredCats = cats.filter(cat => 
+        cat.name.toLowerCase().includes(searchField.toLowerCase())
       );
     return (
       <div className="App">
-        <h1>Monster Rolodex</h1>
+        <h1>Cat Catalog</h1>
         <SearchBox 
-          placeholder="Search Monsters" 
+          placeholder="Got a Certain Kitty in Mind?" 
           handleChange={this.handleChange}
         />
-        <CardList monsters={filteredMonsters} />
+        <CardList cats={filteredCats} />
       </div>
     );
   }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,8 +3,8 @@ import './card.styles.css';
 
 export const Card = (props) => (
   <div className='card-container'>
-    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
-    <h2>{ props.monster.name }</h2>
-    <p>{ props.monster.email }</p>
+    <img alt="kitty" src={`https://robohash.org/${props.cat.id}?set=set4&size=180x180`} />
+    <h2>{ props.cat.name }</h2>
+    <p>{ props.cat.description }</p>
   </div>
 );
